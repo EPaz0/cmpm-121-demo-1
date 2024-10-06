ebb9a428f26bc23b0e8fc6c48a29a5a73954fb1f
EPaz0 <enrique_pazg@yahoo.com>
2024-10-06 16:24:06 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,33 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
+//Button
+const button = document.createElement('button');
+
+//Set button to emoji
+button.innerHTML = '🐵'
+
+button.style.fontSize = '24px';
+
+//Create container div to center the button and header
+//uses flexbox to align the children(header & button)
+// vertically and horizontally
+const container = document.createElement('div');
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+container.style.justifyContent = 'center';
+container.style.alignItems = 'center';
+container.style.height = '100vh'; // viewport height for vertical centering
+
+// Append your elements to the container
+container.appendChild(header);
+container.appendChild(button);
+
+
+
+//connect button tot he body of document
+//document.body.appendChild(button);
+// Append the container to the app element
+app.appendChild(container);
