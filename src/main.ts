b7396e412b5cd1b352d6b769a5be05a497944afd
EPaz0 <enrique_pazg@yahoo.com>
2024-10-06 17:18:15 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -37,11 +37,22 @@ const counterDisplay = document.createElement("div");
 
 //counterDisplay.innerHTML = '${counter} Bananas'
 
-// Increment the counter by 1 every second
-setInterval(() => {
-  counter++;
-  counterDisplay.innerHTML = `${counter} Bananas`;
-}, 1500); //Auto clickking every 1.5 seconds
+
+
+let lastTime = performance.now();
+
+function animate(time: number){
+    const deltaTime = time - lastTime; // Calculate elapsed time since the last frame
+    lastTime = time;
+
+    counter += deltaTime / 1000; //Increase by deltaTime in seconds
+   // counterDisplay.innerHTML = `${counter.toFixed(0)} Bananas`;//DIsplay two decimnal places
+
+    requestAnimationFrame(animate); // Request the next frame
+}
+
+requestAnimationFrame(animate);//start aniamtion loop
+
 
 function updateCounter() {
   counter++;
