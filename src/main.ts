6ce40b46104cba352eae5dc928af3d173b370a26
EPaz0 <enrique_pazg@yahoo.com>
2024-10-12 18:12:19 -0700

    Step 9 complete - Restructured code with data-driven design

--- src/main.ts
+++ src/main.ts
@@ -9,67 +9,36 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let counter: number = 0;
+interface Item{
+  name: string;
+  cost: number;
+  growthRate: number;
+  count: number;
+};
+
+const avaibleItems: Item[] = [
+  {name: "🌴 Tier I Upgrade", cost: 10, growthRate: 0.1, count: 0},
+  {name: "🏢 Tier II Upgrade", cost: 100, growthRate: 1, count: 0},
+  {name: "🚀 Tier III Upgrade", cost: 1000, growthRate: 10, count: 0}
+
+];
+//Global variables
+let counter: number = - 0;
 let growthRate: number = 0;
-let upgradeOneCounter: number = 0;
-let upgradeTwoCounter: number = 0;
-let upgradeThreeCounter: number = 0;
+const counterDisplay = document.createElement("div");
+const growthRateDisplay = document.createElement("div");
+let lastTime: number = performance.now();
 
-let upgradeOneCost: number = 10;
-let upgradeTwoCost: number = 100;
-let upgradeThreeCost: number = 1000;
 
-//Button
-const button = document.createElement("button");
+//Main Button Container
+const mainContainer = document.createElement("div");
+mainContainer.style.display = "flex";
+mainContainer.style.flexDirection = "column";
+mainContainer.style.alignItems = "center";
+mainContainer.style.marginTop = "20px";
 
-const buttonUpgrade = document.createElement("button") as HTMLButtonElement;
-const buttonUpgradeTwo = document.createElement("button") as HTMLButtonElement;
-const buttonUpgradeThree = document.createElement(
-  "button",
-) as HTMLButtonElement;
 
-//Set button to emoji
-button.innerHTML = "🐵";
-
-buttonUpgrade.innerHTML = "🌴 Tier I Upgrade - " + upgradeOneCost;
-buttonUpgrade.disabled = true; // Disable the upgrade button initially'
-
-buttonUpgradeTwo.innerHTML = "🏢 Tier II Upgrade - " + upgradeTwoCost;
-buttonUpgradeTwo.disabled = true; // Disable the upgrade button initially'
-
-buttonUpgradeThree.innerHTML = "👽 Tier III Upgrade - " + upgradeThreeCost;
-buttonUpgradeThree.disabled = true; // Disable the upgrade button initially'
-
-button.style.fontSize = "24px";
-
-// Upgrade Counters
-const upgradeOneCounterDisplay = document.createElement("span");
-upgradeOneCounterDisplay.innerHTML = "0";
-upgradeOneCounterDisplay.style.marginLeft = "10px";
-
-const upgradeTwoCounterDisplay = document.createElement("span");
-upgradeTwoCounterDisplay.innerHTML = "0";
-upgradeTwoCounterDisplay.style.marginLeft = "10px";
-
-const upgradeThreeCounterDisplay = document.createElement("span");
-upgradeThreeCounterDisplay.innerHTML = "0";
-upgradeThreeCounterDisplay.style.marginLeft = "10px";
-
-//Create container div to center the button and header
-//uses flexbox to align the children(header & button)
-// vertically and horizontally
-const container = document.createElement("div");
-container.style.display = "flex";
-container.style.flexDirection = "column";
-container.style.justifyContent = "center";
-container.style.alignItems = "center";
-container.style.height = "100vh"; // viewport height for vertical centering
-
-// Append your elements to the containerz
-container.appendChild(header);
-container.appendChild(button);
-
-//Create container to center and place future upgrades on the right
+//Upgrade Container
 const upgradeContainer = document.createElement("div");
 upgradeContainer.style.position = "absolute";
 upgradeContainer.style.left = "0";
@@ -78,129 +47,92 @@ upgradeContainer.style.transform = "translate(0, -50%)";
 upgradeContainer.style.display = "flex";
 upgradeContainer.style.flexDirection = "column";
 upgradeContainer.style.justifyContent = "center";
-
-// Create sub-containers for each upgrade button and counter
-const upgradeButtonContainerOne = document.createElement("div");
-upgradeButtonContainerOne.style.display = "flex";
-upgradeButtonContainerOne.style.alignItems = "center";
-upgradeButtonContainerOne.appendChild(buttonUpgrade);
-upgradeButtonContainerOne.appendChild(upgradeOneCounterDisplay);
-
-const upgradeButtonContainerTwo = document.createElement("div");
-upgradeButtonContainerTwo.style.display = "flex";
-upgradeButtonContainerTwo.style.alignItems = "center";
-upgradeButtonContainerTwo.appendChild(buttonUpgradeTwo);
-upgradeButtonContainerTwo.appendChild(upgradeTwoCounterDisplay);
-
-const upgradeButtonContainerThree = document.createElement("div");
-upgradeButtonContainerThree.style.display = "flex";
-upgradeButtonContainerThree.style.alignItems = "center";
-upgradeButtonContainerThree.appendChild(buttonUpgradeThree);
-upgradeButtonContainerThree.appendChild(upgradeThreeCounterDisplay);
-
-// Append the sub-containers to the upgrade container
-upgradeContainer.appendChild(upgradeButtonContainerOne);
-upgradeContainer.appendChild(upgradeButtonContainerTwo);
-upgradeContainer.appendChild(upgradeButtonContainerThree);
-
-const counterDisplay = document.createElement("div");
-const growthRateDisplay = document.createElement("div");
-
-//counterDisplay.innerHTML = '${counter} Bananas'
-
-let lastTime = performance.now();
-
-function animate(time: number) {
-  const deltaTime = time - lastTime; // Calculate elapsed time since the last frame
-  lastTime = time;
-
-  counter += (growthRate * deltaTime) / 1000; //Increase by deltaTime in seconds
-  counterDisplay.innerHTML = `${counter.toFixed(0)} 🍌 Bananas`; //DIsplay two decimnal places
-  growthRateDisplay.innerHTML = `${growthRate.toFixed(2)} per second`;
-
-  // Enable upgrade button if the player has at least 10 units
-  buttonUpgrade.disabled = counter < upgradeOneCost;
-  buttonUpgradeTwo.disabled = counter < upgradeTwoCost;
-  buttonUpgradeThree.disabled = counter < upgradeThreeCost;
-
-  requestAnimationFrame(animate); // Request the next frame
-}
-
-requestAnimationFrame(animate); //start aniamtion loop
-
-function updateCounter() {
-  counter++;
-  if (counter == 1) {
-    counterDisplay.innerHTML = `${counter} Banana`;
-  } else {
-    counterDisplay.innerHTML = `${counter} Bananas`;
-  }
-}
-
-// Upgrade purchase logic
-function purchaseUpgradeOne() {
-  if (counter >= upgradeOneCost) {
-    counter -= upgradeOneCost; // Deduct 10 units
-    growthRate = growthRate + 0.1; // Increase the growth rate
-    counterDisplay.innerHTML = `${Math.floor(counter)} Bananas`;
-
-    upgradeOneCounter++;
-    upgradeOneCounterDisplay.innerHTML = upgradeOneCounter.toString();
-
-    upgradeOneCost = Math.floor(upgradeOneCost * 1.5); // Increase the cost of the upgrade
-    buttonUpgrade.innerHTML = `🌴 Tier I Upgrade - ${upgradeOneCost}`;
-
-    // Re-disable if counter falls below 10
-    buttonUpgrade.disabled = counter < upgradeOneCost;
-  }
-}
-// Upgrade purchase logic
-function purchaseUpgradeTwo() {
-  if (counter >= upgradeTwoCost) {
-    counter -= upgradeTwoCost; // Deduct 10 units
-    growthRate = growthRate + 2; // Increase the growth rate
-    counterDisplay.innerHTML = `${Math.floor(counter)} Bananas`;
-
-    upgradeTwoCounter++;
-    upgradeTwoCounterDisplay.innerHTML = upgradeTwoCounter.toString();
-
-    upgradeTwoCost = Math.floor(upgradeTwoCost * 5); // Increase the cost of the upgrade
-    buttonUpgradeTwo.innerHTML = `🌴 Tier II Upgrade - ${upgradeTwoCost}`;
-
-    // Re-disable if counter falls below 10
-    buttonUpgrade.disabled = counter < upgradeTwoCost;
-  }
-}
-//Upgrade three function
-function purchaseUpgradeThree() {
-  if (counter >= upgradeThreeCost) {
-    counter -= upgradeThreeCost; // Deduct 10 units
-    growthRate = growthRate + 50; // Increase the growth rate
-    counterDisplay.innerHTML = `${Math.floor(counter)} Bananas`;
-
-    upgradeThreeCounter++;
-    upgradeThreeCounterDisplay.innerHTML = upgradeThreeCounter.toString();
-
-    upgradeThreeCost = Math.floor(upgradeThreeCost * 100); // Increase the cost of the upgrade
-    buttonUpgradeThree.innerHTML = `🚀 Tier III Upgrade - ${upgradeThreeCost}`;
-
-    // Re-disable if counter falls below 10
-    buttonUpgrade.disabled = counter < upgradeThreeCost;
-  }
-}
-
-// Add event listener to the button
-button.addEventListener("click", updateCounter);
-buttonUpgrade.addEventListener("click", purchaseUpgradeOne);
-buttonUpgradeTwo.addEventListener("click", purchaseUpgradeTwo);
-buttonUpgradeThree.addEventListener("click", purchaseUpgradeThree);
-
-// Append the container to the app element
-container.appendChild(counterDisplay);
-container.appendChild(growthRateDisplay);
-
-//connect button tot he body of document
-//document.body.appendChild(button);
-// Append the container to the app element
-app.appendChild(container);
 app.appendChild(upgradeContainer);
+
+
+//MainButton
+const button = document.createElement("button");
+button.classList.add("monkey-button");
+button.innerHTML = "🐵";
+button.style.fontSize = "300px";
+mainContainer.appendChild(button); // Append the main button to the container
+
+// Counter Display
+counterDisplay.style.marginTop = "20px";
+mainContainer.appendChild(counterDisplay); // Append the counter display to the container
+
+// Growth Rate Display
+growthRateDisplay.style.marginTop = "10px";
+mainContainer.appendChild(growthRateDisplay); // Append the growth rate display to the container
+
+// Append Main Container to App
+app.appendChild(mainContainer);
+
+
+// Add event listener to the main button to increment the counter
+button.addEventListener("click", () => {
+  counter += 1; // Increment counter by 1 for each click
+  counterDisplay.innerHTML = `${Math.floor(counter)} 🍌 Bananas`; // Update the counter display
+});
+
+//Upgrade buttons
+avaibleItems.forEach((item) => {
+  const buttonUpgrade = document.createElement("button");
+  buttonUpgrade.innerHTML = `${item.name} - ${item.cost}`; // Set the button text to the item name
+  buttonUpgrade.disabled = true; // Disable the upgrade button initially
+  buttonUpgrade.style.marginBottom = "10px"; // Add some margin to the bottom of the button
+  
+  //Create counter display for each upgrade
+  const itemCounterDisplay = document.createElement("span");
+  itemCounterDisplay.innerHTML = item.count.toString();
+  itemCounterDisplay.style.marginLeft = "10px";
+
+  //Append button and counter to upgrade container
+  const upgradeButtonContainer = document.createElement("div");
+  upgradeButtonContainer.style.display = "flex";
+  upgradeButtonContainer.style.alignItems = "center";
+  upgradeButtonContainer.appendChild(buttonUpgrade);
+  upgradeButtonContainer.appendChild(itemCounterDisplay);
+  upgradeContainer.appendChild(upgradeButtonContainer);
+
+  //Add event listener to the button
+  buttonUpgrade.addEventListener("click", () => {
+    if(counter >= item.cost)
+    {
+      counter -= item.cost; // Deduct the cost of the upgrade
+      growthRate += item.growthRate; // Increase the growth rate
+      counterDisplay.innerHTML = `${Math.floor(counter)} Bananas`; // Update the counter display
+      item.count++; // Increase the count of the item
+      itemCounterDisplay.innerHTML = item.count.toString(); // Update the item counter display
+
+      // Increase the cost of the upgrade
+      item.cost = Math.floor(item.cost * 1.5);
+      buttonUpgrade.innerHTML = `${item.name} - ${item.cost}`; // Update the button text
+
+      //disable the button if the player can't afford it
+      buttonUpgrade.disabled = counter < item.cost;
+
+    }
+  });
+
+  //Update button enabled state within animation loop
+  function upgradeButtonState() {
+    buttonUpgrade.disabled = counter < item.cost;
+  }
+
+  //Call updateButtonState insdie animation loop
+  function animate(time: number) {
+    
+    const deltaTime = time - lastTime; // Calculate the time since the last frame
+    lastTime = time;
+
+    counter += (growthRate * deltaTime) / 1000; // Increase the counter by the growth rate
+    counterDisplay.innerHTML = `${Math.floor(counter)} 🍌 Bananas`; // Update the counter display
+    growthRateDisplay.innerHTML = `${growthRate.toFixed(1)} per second`; // Update the growth rate display
+
+    upgradeButtonState(); // Update the button state
+    requestAnimationFrame(animate); // Call the animate function again
+  }
+  requestAnimationFrame(animate); // Call the animate function for the first time
+
+});
